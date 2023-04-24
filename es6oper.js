96b82b81cf7e30bec907c7fb0f3720064918f3a2
Moses Nova <mosesabhisheknova@yahoo.com>
2023-04-25 10:56:51 +1200

    secondry

--- es6oper.js
+++ es6oper.js
@@ -1,1 +1,6 @@
 console.log("This IS jAVAsCRIPT ")
+const temp =(() => {return "Intro to Arrow operator and Spread operator"});
+temp();
+console.log(temp());
+const aarray = [10];
+console.log(...aarray);
